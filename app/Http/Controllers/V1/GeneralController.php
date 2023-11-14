4c9d6671874dd86b985c2ecc99965b278b0edab7
yasirraza95 <raza.yasir95@gmail.com>
2023-11-14 21:02:29 +0500

    category namer

--- app/Http/Controllers/V1/GeneralController.php
+++ app/Http/Controllers/V1/GeneralController.php
@@ -582,7 +582,7 @@ class GeneralController extends Controller
     public function listServicebyCategory(Request $request)
     {
         $name = $request->name;
-        $category = Category::select('id')->where('name', 'like', '%' . $name . '%')->first();
+        $category = Category::select('id')->where('name', 'like', '%' . $name . '%')->firstOrFail();
         $result = Service::where('category_id', $category->id)->get();
 
         $counter = count($result);
